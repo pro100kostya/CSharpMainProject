c1ddb1e56f55bdccb6a7f9c80163a61c0afd2988
pro100kostya <kxolmov@gmail.com>
2024-01-08 01:22:53 +0300

    test5

---
+++ Assets/Scripts 2/MyFistScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 3;
+        int j = 2;
+        int k = i - j;
+        
+        Debug.Log(10 / k);
+
+        Debug.Log("Hello");
+    }
+
+
+}
