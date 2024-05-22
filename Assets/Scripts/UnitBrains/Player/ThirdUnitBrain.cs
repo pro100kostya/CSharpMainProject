1a2dc97234cbd744b951e49cca051db76a3351ae
pro100kostya <kxolmov@gmail.com>
2024-05-23 01:17:36 +0300

    homework 8

---
+++ Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
@@ -0,0 +1,77 @@
+using Codice.Client.BaseCommands;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnitBrains.Player;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class ThirdUnitBrain : DefaultPlayerUnitBrain
+{
+    public override string TargetUnitName => "Ironclad Behemoth";
+    private bool IsMoving = false;
+    private bool IsShooting = false;
+    private int MovingCooldownCounter = 0;
+    private int ShootingCooldownCounter = 0;
+
+    private List<Vector2Int> Targets = new List<Vector2Int>();
+    private List<Vector2Int> WithoutTargets = new List<Vector2Int>();
+
+
+    public override Vector2Int GetNextStep()
+    {
+        if (!Targets.Any() && MovingCooldownCounter >= 4)
+        {
+            IsMoving = true;
+            return base.GetNextStep();
+        }
+        else
+        {
+            IsMoving = false;
+            return unit.Pos;
+        }
+    }
+
+
+
+    protected override List<Vector2Int> SelectTargets()
+    {
+        Targets.Clear();
+        Targets = GetReachableTargets();
+
+        if (Targets.Any() && ShootingCooldownCounter >= 2)
+        {
+            IsShooting = true;
+            while (Targets.Count > 1)
+            {
+                Targets.RemoveAt(Targets.Count - 1);
+            }
+            return Targets;
+        }
+        else
+        {
+            IsShooting = false;
+            return WithoutTargets;
+        }
+
+            
+        
+        
+    }
+
+
+
+    public override void Update(float deltaTime, float time)
+    {
+        if (!IsShooting)
+            MovingCooldownCounter++;
+        else
+            MovingCooldownCounter = 0;
+
+        if (!IsMoving)
+            ShootingCooldownCounter++;
+        else
+            ShootingCooldownCounter = 0;
+    }
+}
